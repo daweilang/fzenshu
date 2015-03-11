01af245bcf39cd487376ceaa63497c55583e874a
jianghui <jianghui@infohold.com.cn>
2015-03-11 16:59:04 +0800

    first commit

---
+++ 3rd_lineup/gatag.js
@@ -0,0 +1,259 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja-JP" lang="ja-JP">
+<head>
+
+
+<!-- MOBIFY - DO NOT ALTER - PASTE IMMEDIATELY AFTER OPENING HEAD TAG -->
+<script type="text/javascript">/*<![CDATA[*/(function(a){function b(a,b){if(+a)return~a||(d.cookie=h+"=; path=/");j=d.createElement(e),k=d.getElementsByTagName(e)[0],j.src=a,b&&(j.onload=j.onerror=b),k.parentNode.insertBefore(j,k)}function c(){n.api||b(l.shift()||-1,c)}if(this.Mobify)return;var d=document,e="script",f="mobify",g="."+f+".com/",h=f+"-path",i=g+"un"+f+".js",j,k,l=[!1,1],m,n=this.Mobify={points:[+(new Date)],tagVersion:[6,1],ajs:"../../../a.mobify.com/sol/a.js"},o=/((; )|#|&|^)mobify-path=([^&;]*)/g.exec(location.hash+"; "+d.cookie);o?(m=o[3])&&!+(m=o[2]&&sessionStorage[h]||m)&&(l=[!0,"//preview"+g+escape(m)]):(l=a()||l,l[0]&&l.push("//cdn"+i,"//files01"+i)),l.shift()?(d.write('<plaintext style="display:none;">'),setTimeout(c)):b(l[0])})(function(){if(/ip(hone|od)|android.*(mobile)|blackberry.*applewebkit|bb1\d.*mobile/i.test(navigator.userAgent)){return[1,"../../../cdn.mobify.com/sites/sol/production/mobify.js"]}return[0,Mobify.ajs]})/*]]>*/</script>
+<!-- END MOBIFY -->
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="Content-Style-Type" content="text/css" />
+<meta http-equiv="Content-Script-Type" content="text/javascript" />
+<title>ページが見つかりません。 | 小学館</title>
+<meta name="keywords" content="ページが見つかりません。,小学館" />
+<meta name="description" content="ページが見つかりません。,小学館" />
+
+<link rel="shortcut icon" type="image/x-icon" href="../../favicon.ico" />
+<link rel="alternate" type="application/rss+xml" title="お知らせ" href="../../st/rss/news/all.xml" /><link rel="stylesheet" type="text/css" media="screen, print" href="../../shared/css/import.css" />
+<!-- Common CSS -->
+<!--[if lte IE 6.0]>
+<script type="text/javascript" src="../../shared/js/DD_belatedPNG_0.0.8a-min.js"></script>
+<script type="">
+DD_belatedPNG.fix('div#header ul#nav-global li ul, div#header ul#nav-global li ul li a, div#header ul#nav-global li ul li a img, .tooltip div.tooltip-text p, .tooltip div.tooltip-text p span');
+</script>
+<![endif]-->
+<script type="text/javascript" src="../../shared/js/jquery-1.3.2.min.js"></script>
+<script type="text/javascript" src="../../shared/js/jquery.droppy.js"></script>
+<script type="text/javascript" src="../../shared/js/jquery.set_goods.js"></script>
+<script type="text/javascript" src="../../shared/js/mjl.js"></script>
+<script type="text/javascript" src="../../shared/js/run.js"></script>
+
+<!-- Common Scripts --></head>
+<body>
+<div id="header"><div id="header-inner">
+<p id="site-logo-shogakukan"><a href="../../default.htm"><img src="../../shared/images/logo_shogakukan_01.jpg" alt="小学館" height="94" /></a></p>
+<span id="mobify-back-to-mobile"></span>
+<script type="text/javascript">
+
+(function() {
+    var el;
+    var isMobile = /ip(hone|od)|android.*(mobile)|blackberry.*applewebkit/i.test(navigator.userAgent);
+    var thePast = new Date(1990,1,1);
+    if (isMobile && (el = document.getElementById('mobify-back-to-mobile'))) {
+        console.log('hello');
+        el.innerHTML = '<a href="#" onclick="document.cookie=\'mobify-path=;path=/\;expires=' + thePast.toUTCString() + '\';document.location.reload();return false;"><img src="../../shared/images/btn_tosp.gif" alt="スマホ版へ" /></a>';}})();
+
+</script>
+
+<ul class="nav-utility roll">
+<li><a href="../../news/link"><img src="../../shared/images/btn_nu_07.gif" alt="関連サイト" width="70" height="16" /></a></li>
+<li><a href="../../company"><img height="16" width="68" alt="会社案内" src="../../shared/images/btn_nu_02.gif"/></a><a class="popup" href="../../company/english"><img height="16" width="103" alt="company profile" src="../../shared/images/btn_nu_06.gif"/></a></li>
+<li><a href="../../../jinji.shogakukan.co.jp/default.htm" target="_blank"><img src="../../shared/images/btn_nu_01.gif" alt="採用" width="42" height="16" /></a></li>
+<li><a href="../../news/mailmag"><img src="../../shared/images/btn_nu_04.gif" alt="メルマガ" width="63" height="16" /></a></li>
+<li><a href="../../st/rss/news/all.xml"><img src="../../shared/images/btn_nu_03.gif" alt="RSS" width="45" height="16" /></a></li>
+<li><a href="../../sitemap"><img src="../../shared/images/btn_nu_05.gif" alt="サイトマップ" width="81" height="16" /></a></li>
+<!-- /.nav-utility --></ul>
+<div id="box-search">
+<form action="../../search_result">
+<fieldset>
+<legend>サイト内検索</legend>
+<select id="category" name="qf">
+<option value="1">すべて</option>
+<option value="2">著者・作家</option>
+<option value="3">作品名</option>
+<option value="4">ISBN</option>
+<option value="5">ニュース</option>
+</select>
+<input type="text" value="" id="keyword" name="q" />
+<input type="submit" name="SerchButton" class="btn-search-01" value="" />
+</fieldset>
+</form>
+<ul>
+<li class="first-child"><a href="../../yomi/author">著者・作家を50音からさがす</a></li>
+</ul>
+<!-- /#box-search --></div>
+<!-- /#header-inner --></div>
+
+<ul id="nav-global" class="roll">
+<li><a href="../../default.htm"><img src="../../shared/images/btn_ng_01.jpg" alt="HOME" width="116" height="33" /></a></li>
+<li><a href="../../books"><img src="../../shared/images/btn_ng_02.jpg" alt="書籍" width="116" height="33" /></a>
+<ul>
+<li><a href="../../books/type/_id_100001" id="ng02-01"><img src="../../shared/images/btn_ng_02_01.png" alt="文庫" width="120" height="33" /></a></li>
+<li><a href="../../books/type/_id_100002" id="ng02-02"><img src="../../shared/images/btn_ng_02_02.png" alt="新書" width="120" height="30" /></a></li>
+<li><a href="../../books/type/_id_100006" id="ng02-03"><img src="../../shared/images/btn_ng_02_03.png" alt="ライトノベル" width="120" height="30" /></a></li>
+<li><a href="../../books/search_result" id="ng02-04"><img src="../../shared/images/btn_ng_02_04.png" alt="書籍全般" width="120" height="31" /></a></li>
+<li><a href="../../books/type/_id_100004" id="ng02-05"><img src="../../shared/images/btn_ng_02_05.png" alt="ムック" width="120" height="30" /></a></li>
+<li><a href="../../books/type/_id_100005" id="ng02-06"><img src="../../shared/images/btn_ng_02_06.png" alt="DVDつき" width="120" height="31" /></a></li>
+<li><a href="../../magazines/#anc01" id="ng02-07"><img src="../../shared/images/btn_ng_02_07.png" alt="ウイークリーブック" width="120" height="34" /></a></li>
+</ul>
+</li>
+<li><a href="../../magazines"><img src="../../shared/images/btn_ng_03.jpg" alt="雑誌" width="116" height="33" /></a>
+<ul>
+<li><a href="../../magazines/genre/_id_001000" id="ng03-01"><img src="../../shared/images/btn_ng_03_01.png" alt="ファッション・美容誌" width="120" height="33" /></a></li>
+<li><a href="../../magazines/genre/_id_002000" id="ng03-02"><img src="../../shared/images/btn_ng_03_02.png" alt="情報誌" width="120" height="30" /></a></li>
+<li><a href="../../magazines/genre/_id_003000" id="ng03-03"><img src="../../shared/images/btn_ng_03_03.png" alt="男性コミック誌" width="120" height="30" /></a></li>
+<li><a href="../../magazines/genre/_id_004000" id="ng03-04"><img src="../../shared/images/btn_ng_03_04.png" alt="女性コミック誌" width="120" height="31" /></a></li>
+<li><a href="../../magazines/genre/_id_005000" id="ng03-05"><img src="../../shared/images/btn_ng_03_05.png" alt="児童・学習誌" width="120" height="30" /></a></li>
+<li><a href="../../magazines/genre/_id_006000" id="ng03-06"><img src="../../shared/images/btn_ng_03_06.png" alt="教育・保育誌・その他" width="120" height="30" /></a></li>
+<li><a href="../../magazines/genre/_id_007000" id="ng03-07"><img src="../../shared/images/btn_ng_03_07.png" alt="ウイークリーブック" width="120" height="34" /></a></li>
+</ul>
+</li>
+<li><a href="../../comics"><img src="../../shared/images/btn_ng_04.jpg" alt="コミックス" width="116" height="33" /></a>
+<ul>
+<li><a href="../../comics/series/_genre_110001" id="ng04-01"><img src="../../shared/images/btn_ng_04_01.png" alt="少年まんが" width="120" height="33" /></a></li>
+<li><a href="../../comics/series/_genre_110003" id="ng04-02"><img src="../../shared/images/btn_ng_04_02.png" alt="少女まんが" width="120" height="30" /></a></li>
+<li><a href="../../comics/series/_genre_110002" id="ng04-03"><img src="../../shared/images/btn_ng_04_03.png" alt="青年まんが" width="120" height="30" /></a></li>
+<li><a href="../../comics/series/_genre_110004" id="ng04-04"><img src="../../shared/images/btn_ng_04_04.png" alt="女性まんが" width="120" height="34" /></a></li>
+</ul>
+</li>
+<li><a href="../../dvd"><img src="../../shared/images/btn_ng_05.jpg" alt="アニメ・DVD・CD" width="116" height="33" /></a>
+<ul>
+<li><a href="../../dvd/animation" id="ng05-01"><img src="../../shared/images/btn_ng_05_01.png" alt="アニメDVD" width="120" height="33" /></a></li>
+<li><a href="../../dvd/movie" id="ng05-02"><img src="../../shared/images/btn_ng_05_02.png" alt="映画DVD" width="120" height="30" /></a></li>
+<li><a href="../../dvd/dvdbook" id="ng05-03"><img src="../../shared/images/btn_ng_05_03.png" alt="DVD BOOK" width="120" height="30" /></a></li>
+<li><a href="../../dvd/weeklybook" id="ng05-04"><img src="../../shared/images/btn_ng_05_04.png" alt="ウイークリーブック" width="120" height="31" /></a></li>
+<li><a href="../../dvd/others" id="ng05-05"><img src="../../shared/images/btn_ng_05_05.png" alt="その他DVD・CD" width="120" height="33" /></a></li>
+</ul>
+</li>
+<li><a href="../../character"><img src="../../shared/images/btn_ng_06.jpg" alt="キャラクター" width="116" height="33" /></a></li>
+<li><a href="../../digital"><img src="../../shared/images/btn_ng_07.jpg" alt="ネット・デジタル" width="116" height="33" /></a>
+<ul>
+
+<li><a href="../../digital/genre/_id_1" id="ng07-01"><img src="../../shared/images/btn_ng_07_01.png" alt="電子書籍・アプリ" width="120" height="33" /></a></li>
+<li><a href="../../digital/genre/_id_2" id="ng07-02"><img src="../../shared/images/btn_ng_07_02.png" alt="コミック・ライトノベル" width="120" height="30" /></a></li>
+<li><a href="../../digital/genre/_id_3" id="ng07-03"><img src="../../shared/images/btn_ng_07_03.png" alt="ショッピング" width="120" height="30" /></a></li>
+<li><a href="../../digital/genre/_id_4" id="ng07-04"><img src="../../shared/images/btn_ng_07_04.png" alt="グラビア・写真" width="120" height="31" /></a></li>
+<li><a href="../../digital/genre/_id_5" id="ng07-05"><img src="../../shared/images/btn_ng_07_05.png" alt="知識・情報" width="120" height="31" /></a></li>
+<li><a href="../../digital/genre/_id_6" id="ng07-06"><img src="../../shared/images/btn_ng_07_06.png" alt="教育" width="120" height="36" /></a></li>
+</ul>
+</li>
+<li><a href="../../news"><img src="../../shared/images/btn_ng_08.jpg" alt="お知らせ" width="116" height="33" /></a>
+<ul>
+<li><a href="../../news/event" id="ng08-01"><img src="../../shared/images/btn_ng_08_01.png" alt="サイン会・イベント" width="120" height="33" /></a></li>
+<li><a href="../../news/prize" id="ng08-02"><img src="../../shared/images/btn_ng_08_02.png" alt="各賞・作品募集" width="120" height="30" /></a></li>
+<li><a href="../../news/guide" id="ng08-03"><img src="../../shared/images/btn_ng_08_03.png" alt="テレビ・映画化関連情報" width="120" height="30" /></a></li>
+<li><a href="../../news/mailmaga" id="ng08-04"><img src="../../shared/images/btn_ng_08_04.png" alt="小学館のメルマガ一覧" width="120" height="31" /></a></li>
+<li><a href="../../news/present" id="ng08-05"><img src="../../shared/images/btn_ng_08_05.png" alt="プレゼント" width="120" height="33" /></a></li>
+</ul>
+</li>
+
+<!--/#nav-global --></ul>
+
+<!-- /#header --></div>
+
+
+
+<div id="contents">
+<div id="main-contents" class="lyt-col1">
+
+<p id="topicpath"><a href="../../default.htm">HOME</a> &gt; <em>ページが見つかりません。</em></p>
+<div class="box-divide-04">
+<div class="hdg-text-01">
+<h1>お探しのページが見つかりません</h1>
+<!-- /.hdg-text-01 --></div>
+<!-- /.box-divide-04 --></div>
+
+<p class="mr10 ml10"><strong class="doc-strong-01"><em>指定されたページは見つかりませんでした。</em></strong><br />
+ファイルが削除されているか、アドレスが変更されている可能性があります。</p>
+
+<!-- /#main-contents --></div>
+
+<!-- /#contents --></div>
+
+<div id="footer">
+
+<div id="nav-site">
+<ul>
+<li><a href="../../books">書籍</a>
+<ul>
+<li><a href="../../books/genre/_id_201000">文学・小説</a></li>
+<li><a href="../../books/genre/_id_202000">社会・ビジネス</a></li>
+<li><a href="../../books/genre/_id_203000">旅行・地図</a></li>
+<li><a href="../../books/genre/_id_204000">趣味</a></li>
+<li><a href="../../books/genre/_id_205000">実用・教育</a></li>
+<li><a href="../../books/genre/_id_206000">アート・教養・エンタメ</a></li>
+<li><a href="../../books/genre/_id_207000">事典・図鑑・語学・辞書</a></li>
+<li><a href="../../books/genre/_id_208000">こども</a></li>
+</ul></li>
+<li><a href="../../magazines">雑誌</a>
+<ul>
+<li><a href="../../magazines/genre/_id_001000">ファッション・美容誌</a></li>
+<li><a href="../../magazines/genre/_id_002000">情報誌</a></li>
+<li><a href="../../magazines/genre/_id_003000">男性コミック誌</a></li>
+<li><a href="../../magazines/genre/_id_004000">女性コミック誌</a></li>
+<li><a href="../../magazines/genre/_id_005000">児童・学習誌</a></li>
+<li><a href="../../magazines/genre/_id_006000">教育・保育誌・その他</a></li>
+<li><a href="../../magazines/genre/_id_007000">ウイークリーブック</a></li>
+</ul></li>
+<li><a href="../../comics">コミックス</a>
+<ul>
+<li><a href="../../comics/series/_genre_110001">少年まんが</a></li>
+<li><a href="../../comics/series/_genre_110003">少女まんが</a></li>
+<li><a href="../../comics/series/_genre_110002">青年まんが</a></li>
+<li><a href="../../comics/series/_genre_110004">女性まんが</a></li>
+</ul></li>
+<li><a href="../../dvd">アニメ・DVD・CD</a>
+<ul>
+<li><a href="../../dvd/animation">アニメDVD</a></li>
+<li><a href="../../dvd/movie">映画DVD</a></li>
+<li><a href="../../dvd/dvdbook">DVD BOOK</a></li>
+<li><a href="../../dvd/weeklybook">ウイークリーブック</a></li>
+<li><a href="../../dvd/others">その他DVD・CD</a></li>
+</ul></li>
+<li><a href="../../character">キャラクター</a></li>
+<li><a href="../../digital">ネット・デジタル</a>
+<ul>
+<li><a href="../../digital/genre/_id_1">電子書籍・アプリ</a></li>
+<li><a href="../../digital/genre/_id_2">コミック・ライトノベル</a></li>
+<li><a href="../../digital/genre/_id_3">ショッピング</a></li>
+<li><a href="../../digital/genre/_id_4">グラビア・写真</a></li>
+<li><a href="../../digital/genre/_id_5">知識・情報</a></li>
+<li><a href="../../digital/genre/_id_6">教育</a></li>
+</ul></li>
+<li><a href="../../news">お知らせ</a>
+<ul>
+<li><a href="../../news/event">サイン会・イベント</a></li>
+<li><a href="../../news/prize">各賞・作品募集</a></li>
+<li><a href="../../news/guide">テレビ・映画化関連情報</a></li>
+<li><a href="../../news/mailmaga">小学館のメルマガ一覧</a></li>
+<li><a href="../../news/present">プレゼント</a></li>
+</ul></li>
+</ul>
+<!-- /#nav-site --></div>
+
+<div class="footer-segment">
+<p id="doc-tel" style="height:40px;">小学館愛読者サービスセンター<br />ＴＥＬ　０３－５２８１－３５５５<br/ >　　　　０３－５２８１－３５５６</p>
+<p id="doc-attention"><a href="../../info_attention" class="popup-small">小社や小社の雑誌名を騙るいたずら電話にご注意！</a></p>
+<ul id="nav-related">
+<li id="dora"><a href="../../character/detail/_id_32"><img src="../../shared/images/logo_dora_01.gif" alt="" width="72" height="64" /></a></li>
+
+<li><a href="../../../www.s-book.net/kaku_ri.html" target="_blank"><img src="../../shared/images/logo_s-book_01.gif" alt="" width="69" height="51" /><br /><span>書店様入り口<br />s-book.net</span></a></li>
+<li><a href="../../../www.bookshop-ps.com/default.htm" target="_blank"><img src="../../shared/images/logo_bookshop-ps_01.gif" alt="" width="69" height="51" /><br /><span>本の購入は<br />BOOK SHOP 小学館</span></a></li>
+<li><a href="../../../adpocket.shogakukan.co.jp/default.htm" target="_blank"><img src="../../shared/images/logo_adpocket_01.gif" alt="" width="69" height="51" /><br /><span>小学館広告局<br />AD Pocket</span></a></li>
+<!-- /#nav-related --></ul>
+<!-- /.footer-segment --></div>
+<div class="footer-segment">
+<div class="nav-utility">
+<ul>
+<li class="first-child"><a href="../../inquiry">お問い合わせ</a></li>
+<li><a href="../../picture">画像使用・著作権</a></li>
+<li><a href="../../privacy_policy">プライバシーポリシー/ソーシャルメディアポリシー</a></li>
+<li><a href="../../privacy_policy/cookie">クッキー使用について</a></li>
+</ul>
+
+<!-- /.nav-utility --></div>
+<p id="copyright">掲載の記事・写真・イラスト等すべてのコンテンツの無断複写・転載を禁じます<br />
+<img src="../../shared/images/img_copyright_01.gif" alt="&copy; Shogakukan Inc.  2015 All rights reserved. No reproduction or republication without written permission." width="504" height="11" /></p>
+<!-- /.footer-segment --></div>
+
+<!-- /#footer --></div>
+
+
+<script type="text/javascript" src="../../shared/js/galink.js"></script>
+<script type="text/javascript" src="../../shared/js/matc.js"></script>
+<script type="text/javascript" src="../../shared/js/gatrack.js"></script>
+
+
+</body>
+</html>
